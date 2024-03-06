482b88d47ed53558c0da864391983d6f7b4b1bcb
Azad <AzadPC1324@outlook.com>
2024-03-06 17:51:01 +0330

    .

--- test.php
+++ test.php
@@ -8,60 +8,53 @@ $Users = $Sql->Table("Users");
 $Users = $Users->Select("*");
 $User = $Users->WHERE("chat_id",123456789);
 
-try {
-
-    if (!$User->FirstRow ()) {
-        $Users->Insert()
-            ->Key("chat_id")->Value('123456789')
-            ->Key("first_name")->Value('Mohammad') // Saved as 'mohammad' because the Rebuilder has been used
-            ->Key("last_name")->Value('Azad')  // Saved as 'azad' because the Rebuilder has been used
-            ->Key("salary")->Value('20000000') // Base64 encryption
-        ->End();
-    }
-
-
-    // 10% increase to salary.
-    $NewSalary = $User->WorkOn("salary")->
-        Tool("Percentage")
-            -> Append(10)
-        ->Close()
-    ->Result();
-
-    // Update salary
-    $User->Manage()->Update($NewSalary,"salary");
-
-
-    // Get Salary
-    echo $User->FirstRow ()['salary'];
-    // Result: 22000000
-
-    # ------------------------------------------------------------------- #
-
-    echo PHP_EOL."Load Plugin:".PHP_EOL;
-
-    // Increase salary through plugins
-    $Sql->LoadPlugin ("IncreaseSalary",["chat_id"=>"123456789"])->Increase(10);
-    echo $User->FirstRow ()['salary']; // 24200000
-
-    echo PHP_EOL;
-
-    $User->Manage()->Update(9000,"OneLess");
-    echo $User->FirstRow ()['OneLess']; // in database: 8999 - result: 8999 - 
-    // The reason for -1 from the original number is the type of data type defined for example.
-    echo PHP_EOL;
-
-    $User->Manage()->Increase(150,"OneLess");
-    echo $User->FirstRow ()['OneLess']; // 9148 (8999 + 150 - 1 = 9148)
-    echo PHP_EOL;
-
-    $User->Manage()->Decrease(100,"OneLess");
-    echo $User->FirstRow ()['OneLess']; // 9047 (9148 - 100 - 1 = 9048)
-    echo PHP_EOL;
-
-} catch (\Azad\Conditions\Exception $e) {
-    var_dump($e->Debug);
-    // The value of [USD] is equal to 400 - but you have defined (350) in the EqualTo
+if (!$User->FirstRow ()) {
+    $Users->Insert()
+        ->Key("chat_id")->Value('123456789')
+        ->Key("first_name")->Value('Mohammad') // Saved as 'mohammad' because the Rebuilder has been used
+        ->Key("last_name")->Value('Azad')  // Saved as 'azad' because the Rebuilder has been used
+        ->Key("salary")->Value('20000000') // Base64 encryption
+    ->End();
 }
+
+
+// 10% increase to salary.
+$NewSalary = $User->WorkOn("salary")->
+    Tool("Percentage")
+        -> Append(10)
+    ->Close()
+->Result();
+
+// Update salary
+$User->Manage()->Update($NewSalary,"salary");
+
+
+// Get Salary
+echo $User->FirstRow ()['salary'];
+// Result: 22000000
+
+# ------------------------------------------------------------------- #
+
+echo PHP_EOL."Load Plugin:".PHP_EOL;
+
+// Increase salary through plugins
+$Sql->LoadPlugin ("IncreaseSalary",["chat_id"=>"123456789"])->Increase(10);
+echo $User->FirstRow ()['salary']; // 24200000
+
+echo PHP_EOL;
+
+$User->Manage()->Update(9000,"OneLess");
+echo $User->FirstRow ()['OneLess']; // in database: 8999 - result: 8999 - 
+// The reason for -1 from the original number is the type of data type defined for example.
+echo PHP_EOL;
+
+$User->Manage()->Increase(150,"OneLess");
+echo $User->FirstRow ()['OneLess']; // 9148 (8999 + 150 - 1 = 9148)
+echo PHP_EOL;
+
+$User->Manage()->Decrease(100,"OneLess");
+echo $User->FirstRow ()['OneLess']; // 9047 (9148 - 100 - 1 = 9048)
+echo PHP_EOL;
 
 /*
 INSERT DATA :

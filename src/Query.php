a0a8d20f5370863ecb6bc0fa2290041e3bdb7755
Azad <AzadPC1324@outlook.com>
2024-03-07 17:55:34 +0330

    debug

--- src/Query.php
+++ src/Query.php
@@ -75,7 +75,6 @@ class Query {
             $Query .= $key." = '".$value."'";
             $Query .= ($EndColumn == $key) ? "":" AND ";
         });
-        echo $Query;
         return $Query;
     }
     public static function Insert ($table_name,$data) {

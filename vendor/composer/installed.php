c79610fe98c1a381a3be961c8600f1cddb8c86c0
Azad <AzadPC1324@outlook.com>
2024-03-13 17:21:49 +0330

    .

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'azad1324/sql',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3abc27c01420b7656689f47468ed4918d5cdfb1b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'azad1324/sql' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3abc27c01420b7656689f47468ed4918d5cdfb1b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

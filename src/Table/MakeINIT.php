ec8a819df939042d3c1cd05f2c392510aacd5451
Mohammad Azad <158297225+AzadDevX@users.noreply.github.com>
2024-03-31 01:56:15 +0330

    Update MakeINIT.php

--- src/Table/MakeINIT.php
+++ src/Table/MakeINIT.php
@@ -6,7 +6,7 @@ class MakeINIT extends \Azad\Database\Database {
         self::$SubClass = array_values(array_filter(get_declared_classes(),fn($class_name) => is_subclass_of($class_name,"Azad\Database\Table\Make") && strpos( $class_name, parent::$name_prj[$HashID]."\Tables" ) !== false));
         array_map(function ($class_name) use ($HashID) {
             $class_name = new $class_name();
-            $class_name->Prefix = parent::$TablePrefix[$HashID];
+            if (parent::$is_have_prefix[$HashID]) { $class_name->Prefix = parent::$TablePrefix[$HashID]; }
             $Query = parent::MakeQuery();
             $Query = $Query::MakeTable($class_name);
             preg_match("#(.*)\\\\Tables\\\\(.*)#",get_class($class_name),$table_name);
